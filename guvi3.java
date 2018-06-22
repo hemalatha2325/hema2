6800d1799cf337514a16340d0f18a10134936f3a
hemalatha2325 <40482207+hemalatha2325@users.noreply.github.com>
2018-06-22 12:14:00 +0530

    guvi3.java

---
+++ guvi3.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+class guvi3
+{
+    public static void main(String args[])
+    {
+        char ch='z';
+        if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u')
+        {
+            System.out.println("Vowel");
+        }
+        else
+        {
+            System.out.println("Consonant");
+        }
+    }
+}
