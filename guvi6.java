679197443707bdee449d39cee2dbaf46abd8764d
hemalatha2325 <40482207+hemalatha2325@users.noreply.github.com>
2018-07-07 17:46:12 +0530

    guvi6.java

---
+++ guvi6.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+class guvi6
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        if(a%4==0)
+        {
+            System.out.println("leap");
+        }
+        else
+        {
+            System.out.println("not a leap");
+        }
+    }
+}
