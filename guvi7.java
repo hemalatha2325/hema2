1ab705bff95b38f1b4b82bd0e30e4d2583d37f03
hemalatha2325 <40482207+hemalatha2325@users.noreply.github.com>
2018-07-07 17:48:56 +0530

    guvi7.java

---
+++ guvi7.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+class guvi7
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            System.out.println("Hello");
+        }
+    }
+}
