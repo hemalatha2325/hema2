e4f9b9ee6a913deae578c6d1eb8cff2e1099fe59
hemalatha2325 <40482207+hemalatha2325@users.noreply.github.com>
2018-08-06 17:35:13 +0530

    g21.java

---
+++ g21.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class g21{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int fir=sc.nextInt();
+        int dif=sc.nextInt();
+        int num=sc.nextInt();
+        int s=fir+(num-1)*dif;
+        System.out.print(s);
+        
+    }
+} 
