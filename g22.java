b121d24a086f4fe3ebe9c585c3a7aa6687163714
hemalatha2325 <40482207+hemalatha2325@users.noreply.github.com>
2018-08-06 17:37:41 +0530

    g22.java

---
+++ g22.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+class g22
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        int max=0;
+        for(int i=0;i<n;i++)
+        {
+            if(max<a[i])
+            {
+                max=a[i];
+            }
+        }
+        System.out.println(max);
+    }
+}
+
