9a752eee0be69491a45a1910607614a0e7de243f
hemalatha2325 <40482207+hemalatha2325@users.noreply.github.com>
2018-08-13 15:28:24 +0530

    g34.java

---
+++ g34.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class g34{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        String s1[]=s.split("[.]");
+        int len=s1.length;
+        System.out.print(len);
+    }
+}
