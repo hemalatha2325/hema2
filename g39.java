37182973082ddced8b415a527c8f0fbcabfaa304
hemalatha2325 <40482207+hemalatha2325@users.noreply.github.com>
2018-08-13 17:39:22 +0530

    g39.java

---
+++ g39.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class g39{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int a[]=new int[10];
+        for(int i=0;i<10;i++){
+            a[i]=sc.nextInt();
+        }
+        Arrays.sort(a);
+        System.out.print(a[9]);
+    }
+}
