7df4ba6fdd1278f47ba5316829f4a00e711180d2
hemalatha2325 <40482207+hemalatha2325@users.noreply.github.com>
2018-08-14 15:13:13 +0530

    Create g44.java

---
+++ g44.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class g44{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        if(n>=0 && n<=9){
+            System.out.print("YES");
+        }
+        else{
+            System.out.print("NO");
+        }
+    }
+}
