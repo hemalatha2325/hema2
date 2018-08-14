75581a3a2d55ba5b88b50b97afa8b6877f632903
hemalatha2325 <40482207+hemalatha2325@users.noreply.github.com>
2018-08-14 14:54:30 +0530

    g43.java

---
+++ g43.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class g43{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        String s=sc.next();
+        String s1=sc.next();
+        String s2=s+s1;
+        System.out.print(s2);
+    }
+}
