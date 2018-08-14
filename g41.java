2f5255c80832c48fb2429658250001fe8c275778
hemalatha2325 <40482207+hemalatha2325@users.noreply.github.com>
2018-08-14 14:11:36 +0530

    g41.java

---
+++ g41.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class g41{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        String s=sc.next();
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++){
+            System.out.println(s);
+        }
+    }
+}
