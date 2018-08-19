8a9179937de2ca17cbf483b2b31c3e8a41bbd250
hemalatha2325 <40482207+hemalatha2325@users.noreply.github.com>
2018-08-19 12:22:00 +0530

    g52.java

---
+++ g52.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class g52{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        String s[]={"","One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten"};
+        System.out.print(s[n]);
+    }
+}
