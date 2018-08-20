c50d7784d9c9ce75a6104e63b5ca689e537c1b7e
hemalatha2325 <40482207+hemalatha2325@users.noreply.github.com>
2018-08-20 13:37:26 +0000

    g55.java

---
+++ g55.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class g55{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int m=sc.nextInt();
+        int x=m+n;
+        if(x%2==0){
+            System.out.print("even");
+        }
+        else{
+            System.out.print("odd");
+        }
+    }
+}
