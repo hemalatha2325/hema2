0731973f07d2f000d80aad06ccd49da9b3613472
hemalatha2325 <40482207+hemalatha2325@users.noreply.github.com>
2018-08-21 13:55:34 +0530

    g60.java

---
+++ g60.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class g60{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int a=0;
+        for(int i=1;i<=n;i++){
+            a=a+i;
+        }
+        System.out.print(a);
+    }
+}
+
