726456f9f16142b4d5bda77d49119d29784c6392
hemalatha2325 <40482207+hemalatha2325@users.noreply.github.com>
2018-08-21 13:54:26 +0530

    g59.java

---
+++ g59.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class g59{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        String s=sc.next();
+        int len=s.length();
+        System.out.print(len);
+    }
+}
