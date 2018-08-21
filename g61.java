d2c1d25e258b7ed1bae219312b4e9411c1ad6ef2
hemalatha2325 <40482207+hemalatha2325@users.noreply.github.com>
2018-08-21 13:56:58 +0530

    g61.java

---
+++ g61.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class g61{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        String s=sc.next();
+        int n=sc.nextInt();
+        String ss=s.substring(0,n);
+        System.out.print(ss);
+    }
+}
