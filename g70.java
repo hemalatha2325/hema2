b4645f8b1bdce5fb49c3ae6ebb3751791948c91c
hemalatha2325 <40482207+hemalatha2325@users.noreply.github.com>
2018-08-21 17:44:51 +0530

    g70.java

---
+++ g70.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class g70{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.print(n*2);
+    }
+}
