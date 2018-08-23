1be550ef60de6130263842545d83b7e41627c27d
hemalatha2325 <40482207+hemalatha2325@users.noreply.github.com>
2018-08-23 19:17:57 +0530

    g89.java

---
+++ g89.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class g89{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        String s=sc.next();
+        char a[]=s.toCharArray();
+        Arrays.sort(a);
+        System.out.print(a);
+    }
+}
