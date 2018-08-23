a70d69d4b33b047b5b972a3908453e45f131dfa5
hemalatha2325 <40482207+hemalatha2325@users.noreply.github.com>
2018-08-23 19:10:05 +0530

    g84.java

---
+++ g84.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class g84{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        String s=sc.next();
+        char a=s.charAt(0);
+        //int x=a;
+        int m=(int)a;
+        System.out.print(m);
+    }
+}
