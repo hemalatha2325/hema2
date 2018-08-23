0f45ff9460677611ff0e2d116a94331392c6edd1
hemalatha2325 <40482207+hemalatha2325@users.noreply.github.com>
2018-08-23 19:23:56 +0530

    g93.java

---
+++ g93.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class g93{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        float n=sc.nextFloat();
+        int m=(int)Math.abs(n+273.15);
+        System.out.print(m);
+    }
+}
