6a29c98cf70f898c810d4edf51471e3bbd781425
hemalatha2325 <40482207+hemalatha2325@users.noreply.github.com>
2018-08-23 19:06:57 +0530

    g82.java

---
+++ g82.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class g82{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        float a=sc.nextFloat();
+        float b=sc.nextFloat();
+        float c=a*b;
+        System.out.printf("%.5f",c);
+    }
+}
+
