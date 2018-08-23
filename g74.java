f6b83ff8c48b67a42dc968a0dc3ef9585c3f8c37
hemalatha2325 <40482207+hemalatha2325@users.noreply.github.com>
2018-08-23 18:54:43 +0530

    g74.java

---
+++ g74.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.lang.*;
+class g74{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        float f=sc.nextFloat();
+        int m=Math.round(f);
+        System.out.print(m);
+    }
+}
