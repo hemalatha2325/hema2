a13feabeeac6bdbe32364b39966be3d5ab968963
hemalatha2325 <40482207+hemalatha2325@users.noreply.github.com>
2018-08-23 19:26:12 +0530

    g95.java

---
+++ g95.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class g95{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int p=sc.nextInt();
+        int r=sc.nextInt();
+        int t=sc.nextInt();
+        int m=(p*t*r)/100;
+        System.out.print(m);
+    }
+}
